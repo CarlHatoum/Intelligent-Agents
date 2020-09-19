07f607a681996b9219a40f85c69d81cf1db57b6c
RaphLins <raphael.secondaire@gmail.com>
2020-09-19 19:52:34 +0200

    getters/setters, grassSpace, dislay surf

--- rabbits/sciper1-sciper2/src/RabbitsGrassSimulationModel.java
+++ rabbits/sciper1-sciper2/src/RabbitsGrassSimulationModel.java
@@ -1,6 +1,7 @@
 import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimModelImpl;
 import uchicago.src.sim.engine.SimInit;
+import uchicago.src.sim.gui.DisplaySurface;
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -8,51 +9,125 @@ import uchicago.src.sim.engine.SimInit;
  * order to run Repast simulation. It manages the entire RePast
  * environment and the simulation.
  *
- * @author 
+ * @author
  */
 
 
-public class RabbitsGrassSimulationModel extends SimModelImpl {		
+public class RabbitsGrassSimulationModel extends SimModelImpl {
+	private static final int GRID_SIZE = 20;
+	private static final int NUM_INIT_RABBITS = 0;
+	private static final int NUM_INIT_GRASS = 0;
+	private static final int GRASS_GROWTH_RATE = 0;
+	private static final int BIRTH_THRESHOLD = 0;
 
-		public static void main(String[] args) {
-			
-			System.out.println("Rabbit skeleton");
+    private int gridSize = GRID_SIZE;
+    private int numInitRabbits = NUM_INIT_RABBITS;
+    private int numInitGrass = NUM_INIT_GRASS;
+	private int grassGrowthRate = GRASS_GROWTH_RATE;
+	private int birthThreshold = BIRTH_THRESHOLD;
 
-			SimInit init = new SimInit();
-			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
-			// Do "not" modify the following lines of parsing arguments
-			if (args.length == 0) // by default, you don't use parameter file nor batch mode 
-				init.loadModel(model, "", false);
-			else
-				init.loadModel(model, args[0], Boolean.parseBoolean(args[1]));
-			
+	private Schedule schedule;
+	private RabbitsGrassSimulationSpace space;
+	private DisplaySurface displaySurf;
+
+    public static void main(String[] args) {
+
+        System.out.println("Rabbit skeleton");
+
+        SimInit init = new SimInit();
+        RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+        // Do "not" modify the following lines of parsing arguments
+        if (args.length == 0) // by default, you don't use parameter file nor batch mode
+            init.loadModel(model, "", false);
+        else
+            init.loadModel(model, args[0], Boolean.parseBoolean(args[1]));
+
+    }
+
+    public void begin() {
+		buildModel();
+		buildSchedule();
+		buildDisplay();
+
+		displaySurf.display();
+    }
+
+	public void buildModel(){
+    	space = new RabbitsGrassSimulationSpace(gridSize);
+    	space.initGrass(numInitGrass);
+	}
+
+	public void buildSchedule(){
+	}
+
+	public void buildDisplay(){
+	}
+
+
+    public String[] getInitParam() {
+        // TODO Auto-generated method stub
+        // Parameters to be set by users via the Repast UI slider bar
+        // Do "not" modify the parameters names provided in the skeleton code, you can add more if you want
+        String[] params = {"GridSize", "NumInitRabbits", "NumInitGrass", "GrassGrowthRate", "BirthThreshold"};
+        return params;
+    }
+
+    public String getName() {
+        return "Rabbits Grass Simulation";
+    }
+
+	public Schedule getSchedule(){
+		return schedule;
+	}
+
+    public void setup() {
+        space = null;
+
+		if (displaySurf != null){
+			displaySurf.dispose();
 		}
+		displaySurf = null;
+		displaySurf = new DisplaySurface(this, "Rabbits Grass Simulation 1");
+		registerDisplaySurface("Rabbits Grass Simulation 1", displaySurf);
+    }
 
-		public void begin() {
-			// TODO Auto-generated method stub
-			
-		}
+	public int getGridSize() {
+		return gridSize;
+	}
 
-		public String[] getInitParam() {
-			// TODO Auto-generated method stub
-			// Parameters to be set by users via the Repast UI slider bar
-			// Do "not" modify the parameters names provided in the skeleton code, you can add more if you want 
-			String[] params = { "GridSize", "NumInitRabbits", "NumInitGrass", "GrassGrowthRate", "BirthThreshold"};
-			return params;
-		}
+	public void setGridSize(int gridSize) {
+		this.gridSize = gridSize;
+	}
 
-		public String getName() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public int getNumInitRabbits() {
+		return numInitRabbits;
+	}
 
-		public Schedule getSchedule() {
-			// TODO Auto-generated method stub
-			return null;
-		}
+	public void setNumInitRabbits(int numInitRabbits) {
+		this.numInitRabbits = numInitRabbits;
+	}
 
-		public void setup() {
-			// TODO Auto-generated method stub
-			
-		}
+	public int getNumInitGrass() {
+		return numInitGrass;
+	}
+
+	public void setNumInitGrass(int numInitGrass) {
+		this.numInitGrass = numInitGrass;
+	}
+
+	public int getGrassGrowthRate() {
+		return grassGrowthRate;
+	}
+
+	public void setGrassGrowthRate(int grassGrowthRate) {
+		this.grassGrowthRate = grassGrowthRate;
+	}
+
+	public int getBirthThreshold() {
+		return birthThreshold;
+	}
+
+	public void setBirthThreshold(int birthThreshold) {
+		this.birthThreshold = birthThreshold;
+	}
 }
--- rabbits/sciper1-sciper2/src/RabbitsGrassSimulationSpace.java
+++ rabbits/sciper1-sciper2/src/RabbitsGrassSimulationSpace.java
@@ -1,8 +1,23 @@
+import uchicago.src.sim.space.Object2DGrid;
+
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
  * @author 
  */
 
 public class RabbitsGrassSimulationSpace {
+    private Object2DGrid grassSpace;
 
+    public RabbitsGrassSimulationSpace(int gridSize){
+        grassSpace = new Object2DGrid(gridSize, gridSize);
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0; j < gridSize; j++){
+                grassSpace.putObjectAt(i,j, 0);
+            }
+        }
+    }
+
+    public void initGrass(int number){
+        // TODO
+    }
 }
